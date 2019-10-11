1e5122a3bc172aefc6b283983df4b6ed20499402
Genshi0916 <zerogen2212@gmail.com>
2019-10-11 16:49:33 +0900

    [追加]ファイルの追加をしました

---
+++ script.js
@@ -0,0 +1,17 @@
+function typing(str = ""){
+    let buf = document.getElementById("typing").innerHTML; //書き込み済みの文字を要素から取得
+    let writed = buf.length; //書き込み済みの文字数を取得
+    let write = "";
+    if(writed < str.length){
+        write = str.charAt(writed); //1文字だけ取得する
+    }else{
+        buf = ""; //今回は何度も繰り返すために内容を消去します
+    }
+    document.getElementById("typing").innerHTML = buf + write; //1文字だけ追加していく
+}
+
+const str = document.getElementById("typing").innerHTML; //書き込む文字を要素から取得
+const delay = 200 //1文字が表示される時間
+
+document.getElementById("typing").innerHTML = "";
+window.setInterval(function(){typing(str);}, delay);
