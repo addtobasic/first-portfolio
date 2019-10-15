049d9b0c78e8cbb5f98c6fc6978e1290e84abec7
Genshi0916 <zerogen2212@gmail.com>
2019-10-15 23:46:23 +0900

    [追加]瞬きの処理をスマホにも対応させました

--- script.js
+++ script.js
@@ -9,6 +9,15 @@ $(function () {
         })
     });
 
+    $(function(){
+        $('#icon')
+        .touchend(function(){
+            $('#icon').attr('src', './images/mabataki.png');
+        })
+        .touchstart(function(){
+            $('#icon').attr('src', './images/genshi.jpg');
+        })
+    });
     // $('#profile').hover(function () {
     //     $(this).css('font-size', '10vh');
     // },
